3c4675b207329133e7b293d55f9ab5ca6e10f61a
omattuy <otavio.attuy@gmail.com>
2018-05-27 12:17:33 -0300

    First methods implemented in order to analyse writing in legal files selected by the user

---
+++ no_more_legalese.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import filedialog
+import csv
+
+def select_file():
+    root = tk.Tk()
+    root.withdraw()
+    file_name = filedialog.askopenfilename()
+    return file_name
+
+def get_list_incorrect_expressions():
+    list_incorrect_expressions = []
+    with open('incorrect_expressions.csv', 'r') as csv_file:
+        list = csv.reader(csv_file, delimiter = ',')
+        for i in list:
+            list_incorrect_expressions.append(i)
+    return list_incorrect_expressions
+
+def check_incorrect_expressions(file_name, list_incorrect_expressions):
+    targetFile = open(file_name, 'r', encoding = 'utf-8')
+    line = targetFile.readline()
+    list_incorrect_expressions_target_file = []
+    while line:
+        for e in list_incorrect_expressions:
+            if line.find(e[0]) != -1:
+                list_incorrect_expressions_target_file.append(e[0])
+        line = targetFile.readline()
+    for e in list_incorrect_expressions_target_file:
+        print(e)
+    targetFile.close()
+
+file_name = select_file()
+list_incorrect_expressions = get_list_incorrect_expressions()
+check_incorrect_expressions(file_name, list_incorrect_expressions)
